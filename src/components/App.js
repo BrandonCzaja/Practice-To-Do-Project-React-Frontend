f88af4eea4e51011fab0187d719afa0306de22d7
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-05 12:10:33 -0600

    App.js
    Create state and function to get data from backend server

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
 import React from "react";
 
 export const App = (props) => {
+  /////////
+  // STATE
+  /////////
+
+  // Create state to hold API data
+  const [todos, setTodos] = React.useState([]);
+
+  //////////////
+  // FUNCTIONS
+  //////////////
+
+  // Create a function to grab the latest list of todos
+  const getTodos = async () => {
+    // Make a request to the backend server
+    const response = await fetch(
+      "https://brandon-czaja-to-do-backend.herokuapp.com/"
+    );
+    // Convert the response data to JSON
+    const data = await response.json();
+    // Assign the response data to the setTodos
+    setTodos(data);
+  };
+
   return <h1>Hello World</h1>;
 };
